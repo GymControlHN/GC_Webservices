c0aa598d060bfee6db12cd4b91ab08255b09ff7a
Karla Estrada <patriciaestrada583@gmail.com>
2019-09-28 14:16:03 -0600

    Creando logeo de aplicacion Web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Patricia
                 </div>
 
                 <div class="links">
