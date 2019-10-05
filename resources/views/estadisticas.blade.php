8ce2465487ecaaea914e2f6f04927e37a384f60a
marfredo <miltonartica1996@gmail.com>
2019-10-05 11:17:20 -0600

    Se agrero la tabla para ver las estadisticas de los clientes que han igresado al gym control.

--- resources/views/estadisticas.blade.php
+++ resources/views/estadisticas.blade.php
@@ -5,9 +5,96 @@
     <header class="fondo" style="max-height: 100px;">
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in">Estadisticas</div>
+                <div class="intro-lead-in">Estadísticas</div>
             </div>
         </div>
     </header>
+
+    <div class="container">
+        <div class="alert alert-dismissable mb-n4" role="alert">
+            <h2 style="all: revert">Lista de Estadísticas</h2>
+            <button type="button"  class="btn btn-warning float-right" data-dismiss="alert" >
+                <i class="fas fa-user-plus"></i>
+            </button>
+
+            <form class="form-inline">
+
+                <div class="form-group mr-sm-4 my-sm-4 ">
+                    <input type="text" class="form-control" id="inputText2" placeholder="Buscar">
+                </div>
+                <button type="submit" class="btn btn-primary my-4 ">Buscar</button>
+            </form>
+            <table class="table  mx-sm-0" style="-moz-box-shadow: 1px 3px 50px 20px rgba(189,178,189,0.76);
+    box-shadow: 1px 3px 50px 20px rgba(189,178,189,0.76);">
+                <thead class="thead-light">
+                <tr>
+                    <th scope="col">N°</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Numero de Cuenta o Identidad</th>
+                    <th scope="col">Edad</th>
+                    <th scope="col">Profesión</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>20162300165</td>
+                    <td>tuto</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>fruta</td>
+                    <td>
+                        <button type="button" class="btn btn-secondary btn-sm">Ver Estadística</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>20162300165</td>
+                    <td>tuto</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>fruta</td>
+                    <td>
+                        <button type="button" class="btn btn-secondary btn-sm">Ver Estadística</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>20162300165</td>
+                    <td>tuto</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>fruta</td>
+                    <td>
+                        <button type="button" class="btn btn-secondary btn-sm">Ver Estadística</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>20162300165</td>
+                    <td>tuto</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>fruta</td>
+                    <td>
+                        <button type="button" class="btn btn-secondary btn-sm">Ver Estadística</button>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 @endsection
