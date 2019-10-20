65a15dbf5018e4b5e241479bd12066b22ec71340
Tere2020 <tereavilez98@gmail.com>
2019-10-20 12:18:03 -0600

    cambio de vistas y modelos  y controladores

--- resources/views/estudiantes.blade.php
+++ resources/views/estudiantes.blade.php
@@ -281,6 +281,7 @@
                         <h5 class="modal-title">Modal title</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
+
                         </button>
                     </div>
                     <div class="modal-body">
@@ -304,6 +305,5 @@
    </div>
 
    </div>
-
 
 @endsection
