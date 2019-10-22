d9d4a9dfbe48f1014415416b2a01775a97222a6e
Osmany <osmanyflores60@gmail..com>
2019-10-21 22:23:46 -0600

    se difinieron las formulsa de ruffier, grasa y imc, ademas se agregaro rutas, y se trabajo con los botones de cancelas, y las tablas de imc, ruffier, y grasa tambien calendarios para cada tabla.

--- resources/views/botongrasa.blade.php
+++ resources/views/botongrasa.blade.php
@@ -14,47 +14,46 @@
     <head>
         <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" >
         <title>FORMULARIO PESO IDEAL</title>
-        <script type="text/javascript">function calcularGRASA(){
-                imc=document.getElementById("imc").value;
-                edad=document.getElementById("edad").value;
-                grasa=((1.2*imc)+(0.23 * edad) -10.8 - 5.4);
+        <script type="text/javascript">function calcularGrasa(){
+                imc= document.getElementById("imc").value;
+                edad= document.getElementById("edad").value;
+
+
+
+                grasa=((1.2*imc)+(0.23*edad)-10.8-5.4);
+
+
                 document.getElementById("grasa").value=grasa.toFixed(2);
 
-                if (grasa<=4 && grasa >= 2 ) {
-                    leyenda="Cantidad minima de grasa ";
-                }
-                else if (grasa<=13 && grasa >= 6) {
-                    leyenda=
-                        "Tienes la condicion de un atleta";
 
-                }
 
-                else if (grasa<=17 && grasa >= 14) {
-                    leyenda=
+                if (grasa >  26) {
+                    leyenda="Estas Obeso";
+                }
+                else if (grasa> 18 ) {
+                    leyenda =
+                        "Tienes que cuidarte y comenzar un plan de perdida de grasa";
+                }
+                else if (grasa> 14 ) {
+                    leyenda =
+                        "Se consideran valores de grasas adecuados, pero no nos podemos descuidar";
+                }
+                else if (grasa> 6 ) {
+                    leyenda =
                         "En forma";
-
-                }
-                else if (grasa<=25 && grasa >= 15) {
-                    leyenda=
-                        "Valores de grasas aceptables, pero no te descuides";
-
                 }
 
-
-
-                else if (grasa<=32 && grasa >= 26) {
-                    leyenda=
-                        "Estas obeso";
-
+                else if (grasa> 2 ) {
+                    leyenda =
+                        "Eres un atleta";
                 }
-
-
                 else{
                     leyenda="Algo salio mal"
+
                 }
+
+
                 document.getElementById("leyenda").value=leyenda;
-
-
 
             }</script>
 
@@ -63,46 +62,54 @@
     <body>
 
     <div class="container">
+
         <form name="f1" id="f1">
             <br><br>
-            <h5 class="label2">Calculo de la Grasa Corporal</h5>
+            <h5 class="label2">Calculo de la grasa corporal</h5>
             <br>
             <form class="form-horizontal" action="/action_page.php">
                 <div class="form-group">
-                    <h6 class=" label2" for="email">IMC</h6>
+                    <h6 class=" label2" for="email">IMC:</h6>
                     <div class="col-sm-10">
                         <input type="number" class="form-control inputtamaño3" id="imc"
-                               name="imc" maxlength="3" placeholder="Ingrese su masa corporal" onkeyup="calcularGRASA()">
+                               name="imc" maxlength="3" placeholder="Ingrese su pulso" onkeyup="calcularGrasa()">
                     </div>
 
                 </div>
 
-                <div class="form-group" id="edad" name="edad">
+                <div class="form-group">
                     <h6 class="label2" for="email">Edad:</h6>
                     <div class="col-sm-10">
                         <input type="number" class="form-control inputtamaño3"
-                               id="edad" name="edad" maxlength="3" placeholder="Ingrese su edad" onkeyup="calcularGRASA()">
+                               id="edad" name="edad" maxlength="3" placeholder="Ingrese su pulso" onkeyup="calcularGrasa()">
                     </div>
                 </div>
 
-                <div class="form-group" id="grasa" name="grasa">
-                    <h6 class="label2" for="email">Grasa:</h6>
+
+
+                <div class="form-group">
+                    <h6 class="label2" for="email">%Grasa:</h6>
                     <div class="col-sm-10">
                         <input type="number" class="form-control inputtamaño3"
-                               id="grasa" name="grasa" maxlength="3"  disabled="true">
+                               id="grasa" name="grasa" maxlength="3"  disabled="true" >
                     </div>
                 </div>
 
-                <div class="form-group" id="diagnostico" name="diagnostico">
+                <div class="form-group">
                     <h6 class="label2" for="email">Diagnostico:</h6>
                     <div class="col-sm-10">
                         <input type="text" class="form-control inputtamaño3"
-                               id="leyenda" name="leyenda" maxlength="50"  disabled="true">
+                               id="leyenda" name="leyenda" maxlength="50" disabled="true">
                     </div>
                 </div>
+
                 <div class="container">
-                    <div class="form-group">
-                        <input type="date" class="form-control3" id="fecha_de_ingreso" name="fecha_de_ingreso"
+
+
+
+                    <h6 class="label2" for="email">Fecha:</h6>
+                    <div class="col-sm-10">
+                        <input type="date" class="form-control inputtamaño3" id="fecha_de_ingreso" name="fecha_de_ingreso"
                                placeholder="Escriba la fecha de ingreso"
                                @isset($estudiante)
                                value="{{$estudiante->fecha_de_ingreso}}"
@@ -112,26 +119,22 @@
 
                 </div>
 
-                <input type="button" class="btn btn-primary my-4 boton" value="Guardar" onclick="calcularGRASA()">
-                <input type="button" class="btn btn-primary my-4 boton3" value="Guardar" onclick="calcelarGRASA()">
+                <div class="container1">
+
+                    <button type="button" class="btn btn-primary my-4 boton"><a style="color: white" href="/ruffiel">Cancelar</a></button>
+                    <button type="button" class="btn btn-primary my-4 boton3">Guardar</button>
+                </div>
+
 
             </form>
         </form>
+
+
+
     </div>
-
-
-
-
-
-
-
-
-
-
 
     </body>
     </html>
-
 
 
 
