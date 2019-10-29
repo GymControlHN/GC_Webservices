3758793c12d7908dac62c7e4dcbf9339a1150b2d
Karla Estrada <patriciaestrada583@gmail.com>
2019-10-29 17:20:59 -0600

    Cambio en la imagen de fondo y arreglo del formulario del inicio de sesion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,11 @@
 
 <div class=" fondo" style="font-family: 'Raleway', sans-serif">
         <div class="container">
-            <div class="intro-lead-in ">Bienvenido al control del gimnasio  </div>
+            <div class="intro-lead-in" >Bienvenido al control del gimnasio
+            </div>
             <div class="intro-heading text-uppercase">UNAHTEC-Danli</div>
         </div>
     </div>
-
-
 @endsection
 
 
