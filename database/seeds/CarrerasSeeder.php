f5f57a884428c9d01bea19dc34b43bcc0b1fe38f
Tere2020 <tereavilez98@gmail.com>
2019-11-17 15:52:25 -0600

    cambio para tabla catalogo carrera

---
+++ database/seeds/CarrerasSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Carrera;
+use Illuminate\Database\Seeder;
+
+class CarrerasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $carrera = new Carrera();
+        $carrera->carrera = "Ninguna";
+        $carrera->save();
+
+        $carrera = new Carrera();
+        $carrera->carrera = "Lic. Informática Administrativa";
+        $carrera->save();
+
+        $carrera = new Carrera();
+        $carrera->carrera = "Ing. Agroindustrial";
+        $carrera->save();
+
+        $carrera = new Carrera();
+        $carrera->carrera = "Lic. Enfermeria";
+        $carrera->save();
+
+        $carrera = new Carrera();
+        $carrera->carrera = "TUATEC";
+        $carrera->save();
+
+        $carrera = new Carrera();
+        $carrera->carrera = "Otros";
+        $carrera->save();
+    }
+}
