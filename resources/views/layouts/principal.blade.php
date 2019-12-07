7477c71683480213001ec6ba7f299a7ce0366629
Karla Estrada <patriciaestrada583@gmail.com>
2019-12-06 18:44:31 -0600

    Modal cerrar sesion

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -66,10 +66,7 @@
                             Usuarios
                         </a>
 
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                        <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#cerrarSesionModal">
                             Cerrar sesion
                         </a>
 
@@ -88,7 +85,26 @@
 @yield('content')
 
 
-
+<div class="modal fade" tabindex="-1" id="cerrarSesionModal" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Advertencia</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>¿Estas seguro que deseas cerrar sesión?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary"   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">Cerrar Sesión</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Footer
 <footer class="footer">
     <div class="container">
