75c7271a0dff7b9d410f94efb5b30e0620816455
Karla Estrada <patriciaestrada583@gmail.com>
2020-02-09 19:59:42 -0600

    Menu para las estadisticas y graficos en ver estadisticas

--- resources/views/verestadistica.blade.php
+++ resources/views/verestadistica.blade.php
@@ -9,17 +9,26 @@
         </div>
     </header>
 
+    <div class="card" style="width: 170px; border: none;background: transparent;margin-left: 50px;margin-top: 5px">
+        <div class="card-header" style="background: transparent;height: 50px;" >
+                <a class="btn btn-default" href="/estadisticas"><span><i class="fa fa-arrow-circle-left"></i></span> Regresar</a>
+        </div>
+    </div>
+
 
 
 
     <div class="w3-container w3-teal mx-5">
 
-            <h2 class="card margencard"  style="all: revert;  border: none;">Datos fisicos</h2>
+            <h5 class="card margencard"  style="all: revert;  border: none;">Estadisticas Datos fisicos</h5>
             <h5>Nombre: {{$cliente->nombre}}</h5>
+        <div class="btn-group " style="margin-left: 50px;" role="group" aria-label="Button group with nested dropdown">
 
+            <a class="btn btn-primary" >Estadisticas</a>
+            <a class="btn btn-secondary" >Grafico</a>
 
+        </div>
 
-        <br><br>
 
 
 
