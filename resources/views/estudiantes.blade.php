ec4abaff7d7c191f1b16d66200e0aecce06595ff
Tere2020 <tereavilez98@gmail.com>
2020-04-16 15:25:51 -0600

    se soluciono que al momento de agregar un cliente y no agregaba la foto el margen de la foto se salia ahora ya no

--- resources/views/estudiantes.blade.php
+++ resources/views/estudiantes.blade.php
@@ -208,7 +208,7 @@
                             <div class="form-row">
                                 <div class="form-group {{ $errors->has('imagen') ? ' has-error' : '' }}col-md-6">
                                     <h6>Imagen del Estudiante</h6>
-                                    <img width="300px" style="max-height:150px; object-fit: contain"  id="previewImagen"
+                                    <img width="200px"   id="previewImagen"
                                          @if($errors->has("imagen")) src="{{old("imagen")}}" @endif onclick="seleccionarImagen(event)"/>
 
                                     <label id="labelImagen" for="imagen" class="btn btn-large"><span><i class="fa fa-user fa-4x"></i></span></label>
